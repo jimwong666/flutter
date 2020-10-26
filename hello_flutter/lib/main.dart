f7a2840bff3fe513ca1a50b458eebacd88303fe2
锤锤 <798165604@qq.com>
2020-10-26 20:05:19 +0800

    add
    add flutter lib

---
+++ hello_flutter/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new MaterialApp(
+      title: "Welcome to wj's Flutter",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Center(
+            child: new Text('Welcome to Flutter'),
+          ),
+        ),
+        body: new Center(
+          child: new Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
+  }
+}
